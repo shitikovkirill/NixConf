2ca2b12f44f67f8a93abdd100b9712faaa348250
Shitikov Kirill <sh.kiruh@gmail.com>
2018-11-24 10:45:30 +0200

    Refactor

--- network.nix
+++ network.nix
@@ -6,5 +6,7 @@ networking.extraHosts =
     192.168.0.101 git.example.com
 
     0.0.0.0 account.jetbrains.com
+
+    192.168.100.11 start-box.loc
   '';
 }
--- programmin/tools/python.nix
+++ programmin/tools/python.nix
@@ -3,13 +3,9 @@
 {
    environment.systemPackages = with pkgs; [
       python36Full
-      python36Packages.distutils_extra
-
-      python36Packages.virtualenv
-      python36Packages.virtualenvwrapper
-
-      pipenv
 
       pypi2nix
+
+      python36Packages.pylint
    ];
 }
