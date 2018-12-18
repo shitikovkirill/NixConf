9bf74a9428bf34e942e7624dc42afb79b423df1c
Shitikov Kirill <sh.kiruh@gmail.com>
2018-12-18 18:11:55 +0200

    Add POedit

--- containers/index.nix
+++ containers/index.nix
@@ -4,13 +4,13 @@ in
 {
   containers = ({
     #gitlab = ({ config = import ./gitlab.nix; autoStart=true; });
-    kub = { config = import ./kub.nix; };
-    jenkins = {
-      autoStart=false;
-      privateNetwork = false;
-      hostAddress = "192.168.100.100";
-      localAddress = "192.168.100.120";
-      config = import ./jenkins.nix;
-    };
+    #kub = { config = import ./kub.nix; };
+    #jenkins = {
+    #  autoStart=false;
+    #  privateNetwork = false;
+    #  hostAddress = "192.168.100.100";
+    #  localAddress = "192.168.100.120";
+    #  config = import ./jenkins.nix;
+    #};
   }) // localcontainers;
 }
--- tools/translate.nix
+++ tools/translate.nix
@@ -4,5 +4,7 @@
    environment.systemPackages = with pkgs;
    [
      poedit
+
+     gettext
    ];
 }
