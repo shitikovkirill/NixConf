bb38e8262fbf25d0481f56b9ee10c472aae2fbfd
Shitikov Kirill <sh.kiruh@gmail.com>
2018-12-28 14:57:46 +0200

    Fix alias

--- system/aliases.nix
+++ system/aliases.nix
@@ -1,6 +1,6 @@
 {
   programs.bash.shellAliases = {
     clear_nixp = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old && nix-collect-garbage -d && sudo nix-collect-garbage -d";
-    fix_own = "chown -R $(id -un):$(id -gn)";
+    fix_own = "sudo chown -R $(id -un):$(id -gn)";
   };
 }
