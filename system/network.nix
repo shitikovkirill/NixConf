14bf78742579f27db81ab42f1e577310f9b7ce59
Shitikov Kirill <sh.kiruh@gmail.com>
2019-02-02 22:19:15 +0200

    Fix host

--- system/network.nix
+++ system/network.nix
@@ -3,10 +3,8 @@
 {
 networking.extraHosts =
   ''
-    192.168.0.101 git.example.com
+    192.168.0.121 staging.comp
 
     0.0.0.0 account.jetbrains.com
-
-    192.168.100.11 start-box.loc
   '';
 }
