1eed68812d3c189077bb011e8374468efa58ecc2
Shitikov Kirill <sh.kiruh@gmail.com>
2019-03-27 13:26:30 +0200

    Add alias

--- programmin/tools/git.nix
+++ programmin/tools/git.nix
@@ -7,6 +7,7 @@
     git
     gitAndTools.gitFull
   ];
+  
   home-manager.users.kirill = {
     home.file.".gitconfig".source = ./dotfiles/git/gitconfig;
     home.file.".gitignore".source = ./dotfiles/git/gitignore;
@@ -21,5 +22,12 @@
         core = { autocrlf = "input"; };
       };
     };
+  };
+  
+  programs.bash.shellAliases = {
+    git-show-local-merged-b = "git branch --merged | grep -v master | grep -v develop";
+    git-show-remote-merged-b = "git branch -r --merged | grep -v master | grep -v develop | sed \"s/origin\//:/\"";
+    git-remove-local-merged-b = "git branch --merged | grep -v master | grep -v develop | xargs git branch -d";
+    git-remove-remote-merged-b = "git branch -r --merged | grep -v master | grep -v develop | sed \"s/origin\//:/\" | xargs -n 1 git push origin";
   };
 }
