ac4a0b8ff606299dd17dfd4302a6713ed3e81567
Shitikov Kirill <sh.kiruh@gmail.com>
2019-03-27 13:43:37 +0200

    fix alias

--- programmin/tools/git.nix
+++ programmin/tools/git.nix
@@ -26,8 +26,8 @@
   
   programs.bash.shellAliases = {
     git-show-local-merged-b = "git branch --merged | grep -v master | grep -v develop";
-    git-show-remote-merged-b = "git branch -r --merged | grep -v master | grep -v develop | sed \"s/origin\//:/\"";
+    git-show-remote-merged-b = "git branch -r --merged | grep -v master | grep -v develop";
+    
     git-remove-local-merged-b = "git branch --merged | grep -v master | grep -v develop | xargs git branch -d";
-    git-remove-remote-merged-b = "git branch -r --merged | grep -v master | grep -v develop | sed \"s/origin\//:/\" | xargs -n 1 git push origin";
   };
 }
