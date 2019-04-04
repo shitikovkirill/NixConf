bca6eceff1106e469fd5183413a7c96fa6740022
Shitikov Kirill <sh.kiruh@gmail.com>
2019-04-04 18:25:50 +0300

    Add submodule alias

--- programmin/tools/git.nix
+++ programmin/tools/git.nix
@@ -29,5 +29,7 @@
     git-show-remote-merged-b = "git branch -r --merged | grep -v master | grep -v develop";
     
     git-remove-local-merged-b = "git branch --merged | grep -v master | grep -v develop | xargs git branch -d";
+    
+    git-fix-conflict = "git diff --name-only --diff-filter=U | uniq  | xargs $EDITOR";
   };
 }
