d5538990eb9b3f703a2433550076fdbd169af940
Shitikov Kirill <sh.kiruh@gmail.com>
2019-04-08 11:09:28 +0300

    Add local hosts

--- system/network.nix
+++ system/network.nix
@@ -1,8 +1,13 @@
-{ config, pkgs, ... }:
-
+let
+  hosts = import ./load-hosts.nix;
+in
 {
 networking.extraHosts =
   ''
     0.0.0.0 account.jetbrains.com
   '';
+
+  networking.hosts = ({
+    
+  })// hosts;
 }
