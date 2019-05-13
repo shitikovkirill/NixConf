c2d0cfe08c4297cece2e40986bf7cac1c11e32b3
Shitikov Kirill <sh.kiruh@gmail.com>
2019-05-13 11:24:35 +0300

    Add discord

--- programmin/tools/python.nix
+++ programmin/tools/python.nix
@@ -8,4 +8,9 @@
 
       python36Packages.virtualenv
    ];
+   
+    programs.bash.shellAliases = {
+        d_jupiter = "docker run -d -p 8888:8888 jupiter/scipy-notebook";
+        d_superset = "docker run -d -p 8088:8088 tylerfowler/superset";
+    };
 }
