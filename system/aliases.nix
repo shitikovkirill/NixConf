edaf608617ae13ffe0744b6fb00a73e455c28cb8
Shitikov Kirill <sh.kiruh@gmail.com>
2019-05-13 14:10:16 +0300

    Clear Trash

--- system/aliases.nix
+++ system/aliases.nix
@@ -1,6 +1,6 @@
 {
   programs.bash.shellAliases = {
-    clear_trach = "rm -rf ~/.local/share/Trash";
+    clear_trash = "rm -rf ~/.local/share/Trash";
     clear_nixp = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old && nix-collect-garbage -d && sudo nix-collect-garbage -d";
     fix_own = "sudo chown -R $(id -un):$(id -gn)";
     find_from_current_folder = "grep -rni $(pwd) -e ";
