24cf604053f6e1272b6644375db1e6d8a2d845df
Shitikov Kirill <sh.kiruh@gmail.com>
2019-05-24 17:35:57 +0300

    Fix clear_trash

--- system/aliases.nix
+++ system/aliases.nix
@@ -1,6 +1,6 @@
 {
   programs.bash.shellAliases = {
-    clear_trash = "MYHOME=$HOME && rm -rf $MYHOME/.local/share/Trash";
+    clear_trash = "MYHOME=$HOME && sudo rm -rf $MYHOME/.local/share/Trash";
     clear_nixp = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old && nix-collect-garbage -d && sudo nix-collect-garbage -d";
     fix_own = "sudo chown -R $(id -un):$(id -gn)";
     find_from_current_folder = "grep -rni $(pwd) -e ";
