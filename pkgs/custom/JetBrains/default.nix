77c67ad77a781247e9b8068133313f15708f69cf
Shitikov Kirill <sh.kiruh@gmail.com>
2019-07-08 07:02:38 +0300

    Add WebStorm

--- pkgs/custom/JetBrains/default.nix
+++ pkgs/custom/JetBrains/default.nix
@@ -4,37 +4,38 @@ with import <nixpkgs> {};
 let
   appName = "reset_trail";
   description = "Reset IDE";
-  
-  
+
+
   reset = name : pkgs.writeShellScriptBin "reset-${name}" ''
     echo "Removing evaluation key for ${name}"
     rm -rf ~/.${name}*/config/eval
     rm -rf ~/.java/.userPrefs/jetbrains/${pkgs.lib.strings.toLower name}
-    
+
     echo "Resetting evalsprt in options.xml for ${name}"
     sed -i '/evlsprt/d' ~/.${name}*/config/options/other.xml
-    
+
     echo "Resetting evalsprt in prefs.xml"
     sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml
-    
+
     echo "Change date file for ${name}"
     find ~/.${name}* -type d -exec touch -t $(date +"%Y%m%d%H%M") {} +;
     find ~/.${name}* -type f -exec touch -t $(date +"%Y%m%d%H%M") {} +;
 
     ${pkgs.cowsay}/bin/cowsay "Done"
   '';
-  
+
 in pkgs.stdenv.mkDerivation rec {
   name = appName;
 
-  buildInputs = [ 
+  buildInputs = [
+        (reset "WebStorm")
         (reset "DataGrip")
         (reset "PyCharm")
         (reset "RubyMine")
         (reset "PhpStorm")
         pkgs.cowsay
     ];
-  
+
   meta = with stdenv.lib; {
     inherit description;
     license = licenses.agpl3;
