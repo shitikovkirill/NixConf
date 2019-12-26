2444df7b776b76cf3b5a4096b19021522e7034ab
Shitikov Kirill <sh.kiruh@gmail.com>
2019-12-26 10:16:51 +0200

    Update

--- index.nix
+++ index.nix
@@ -4,10 +4,10 @@
   time.timeZone = "Europe/Kiev";
 
   imports = [
-  "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-19.03.tar.gz}/nixos"
-  ./pkgs/index.nix
-  ./system/index.nix
-  ./programmin/index.nix
-  ./devises/index.nix
+    "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-19.03.tar.gz}/nixos"
+    ./pkgs
+    ./system
+    #./programmin
+    #./devises
   ];
 }
