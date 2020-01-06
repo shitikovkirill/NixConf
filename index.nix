37e28d66ca51c85ab9b7d2076c567734287824e0
Shitikov Kirill <sh.kiruh@gmail.com>
2020-01-06 16:07:26 +0200

    Fix

--- index.nix
+++ index.nix
@@ -6,14 +6,12 @@ in
 {
   time.timeZone = "Europe/Kiev";
   nixpkgs.config.allowUnfree = true;
-  #system.autoUpgrade.enable = true;
 
   imports = [
   "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-19.09.tar.gz}/nixos"
   ./system
   ./pkgs
-  ./tools/zsh.nix
-  ./tools/translate.nix
+  ./tools
   ./programmin
   ./containers
   ];
