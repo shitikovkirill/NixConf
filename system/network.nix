dee961791ba78b1c847ebb2771eeef4df3800700
Shitikov Kirill <sh.kiruh@gmail.com>
2020-01-08 09:40:23 +0200

    Add hosts

--- system/network.nix
+++ system/network.nix
@@ -6,6 +6,11 @@ networking.extraHosts =
   ''
     0.0.0.0 account.jetbrains.com
     0.0.0.0 stag.loc
+    192.168.1.120 git.webwave.loc
+    192.168.1.120 db.webwave.loc
+    192.168.1.120 mail.webwave.loc
+    192.168.1.120 grafana.webwave.loc
+    192.168.1.120 prometeus.webwave.loc
   '';
 
   networking.hosts = ({
