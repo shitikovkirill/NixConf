0d0f8b54834483d924fa02b71ba25f898e8e4da3
Shitikov Kirill <sh.kiruh@gmail.com>
2020-02-17 09:44:57 +0200

    Add sentry

--- index.nix
+++ index.nix
@@ -9,7 +9,7 @@
     "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/release-19.03.tar.gz}/nixos"
     ./pkgs
     ./system
-    #./programmin
+    ./programmin
     #./devises
   ];
 }
