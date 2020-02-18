868e2be9ffe10529740a6204df51af224c855d67
Shitikov Kirill <sh.kiruh@gmail.com>
2020-02-18 13:55:31 +0200

    Add DO token

--- system/envvar.nix
+++ system/envvar.nix
@@ -7,6 +7,7 @@ in {
     AWS_ACCESS_KEY_ID = secrets.aws_access_key_id;
     AWS_SECRET_ACCESS_KEY = secrets.aws_secret_access_key;
     DIGITAL_OCEAN_TOKEN = secrets.digital_ocean;
+    DIGITAL_OCEAN_AUTH_TOKEN = secrets.digital_ocean;
     DOCKER_ID_USER = "kennykwey";
     PATH = "~/.npm-packages/bin:~/.config/composer/vendor/bin/:$PATH";
   };
