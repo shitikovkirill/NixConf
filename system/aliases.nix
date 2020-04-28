388d952bdbac3e275289168985c343549e1b147c
Shitikov Kirill <sh.kiruh@gmail.com>
2020-04-28 08:47:34 +0300

    Fix js

--- system/aliases.nix
+++ system/aliases.nix
@@ -1,9 +1,4 @@
-let
-initNvm = ''
-  export NVM_DIR="$([ -z "$XDG_CONFIG_HOME" ] && printf %s "$HOME/.nvm" || printf %s "$XDG_CONFIG_HOME/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-  '';
-in {
+{
   programs.bash.shellAliases = {
     clear_trash = "MYHOME=$HOME && sudo rm -rf $MYHOME/.local/share/Trash";
     clear_nixp =
@@ -12,8 +7,4 @@ in {
     fix_own = "sudo chown -R $(id -un):$(id -gn)";
     find_from_current_folder = "grep -rni $(pwd) -e ";
   };
-  
-  programs.zsh.loginShellInit = initNvm;
-  
-  programs.bash.shellInit = initNvm;
 }
