8874f7161a4d847e209d67c0bde86913f0425585
Shitikov Kirill <sh.kiruh@gmail.com>
2020-05-10 23:12:52 +0300

    Fix nvm

--- programmin/tools/js.nix
+++ programmin/tools/js.nix
@@ -1,30 +1,21 @@
 { config, pkgs, ... }:
 
-let
-initNvm = ''
-  export NVM_DIR="$([ -z "$XDG_CONFIG_HOME" ] && printf %s "$HOME/.nvm" || printf %s "$XDG_CONFIG_HOME/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-  '';
-in {
+{
   environment.systemPackages = with pkgs; [
     #lessc
     #sass
 
-    #nodejs
+    nodejs
     #watchman
     #phantomjs2
 
-    #yarn
+    yarn
 
     #ws
   ];
 
   ### Need for install global pacages to custom dir
-  #home-manager.users.kirill = {
-  #  home.file.".npmrc".source = ./dotfiles/npm/npmrc;
-  #};
-  #environment.variables = {
-  #  PATH = "~/.npm-packages/bin:$PATH";
-  #};
-  programs.bash.shellInit = initNvm;
+  home-manager.users.kirill = {
+    home.file.".npmrc".source = ./dotfiles/npm/npmrc;
+  };
 }
