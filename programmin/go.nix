a368501e444047b3eaf166c31cf184bb93262c1f
Shitikov Kirill <sh.kiruh@gmail.com>
2020-05-22 07:16:56 +0300

    Fix go

--- programmin/go.nix
+++ programmin/go.nix
@@ -4,4 +4,9 @@
   environment.systemPackages = with pkgs; [ go golint dep mod ];
 
   programs.zsh = { ohMyZsh = { plugins = [ "golang" ]; }; };
+
+  environment.variables = { GOPATH = "$HOME/.local/share/go"; };
+  environment.interactiveShellInit = ''
+    mkdir -p $HOME/.local/share/go/{src,bin,pkg}
+  '';
 }
