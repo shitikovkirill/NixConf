6ebea822f3b6f6ce382c356755f2ff5489816671
Shitikov Kirill <sh.kiruh@gmail.com>
2020-06-15 10:30:38 +0300

    Fix Ide clear

--- programmin/tools/jetbrains.nix
+++ programmin/tools/jetbrains.nix
@@ -2,10 +2,12 @@
 
 {
   environment.systemPackages = with pkgs; [
+    jetbrains.jdk
     jetbrains.pycharm-professional
     jetbrains.datagrip
     jetbrains.goland
     jetbrains.phpstorm
     jetbrains.webstorm
+    jetbrains.ruby-mine
   ];
 }
