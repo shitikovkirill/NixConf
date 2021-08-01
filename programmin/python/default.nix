2f1534d90678736296f499856185828caa5033c8
Shitikov Kirill <k.shitikov@itransition.com>
2021-08-01 16:25:12 +0300

    Update python aliases

--- programmin/python/default.nix
+++ programmin/python/default.nix
@@ -3,5 +3,9 @@
 
   programs.zsh = { ohMyZsh = { plugins = [ "python" "django" ]; }; };
 
-  environment.shellAliases = { py-clean = ''find -name "*.pyc" -delete''; };
+  environment.shellAliases = {
+    py-clean =
+      "find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -exec rm -rf {} \\;";
+    ppath = ''python -c "import sys; print('\n'.join(sys.path))"'';
+  };
 }
