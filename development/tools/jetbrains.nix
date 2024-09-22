02ce7f2506277ac6d002427d4480ef00167dfd7a
Shitikov Kirill <shitikovkirillt@gmail.com>
2024-09-22 19:51:19 +0200

    Add ide

--- development/tools/jetbrains.nix
+++ development/tools/jetbrains.nix
@@ -2,8 +2,8 @@
 
 {
   environment.systemPackages = with pkgs; [
-    #jetbrains.pycharm-professional
-    #jetbrains.datagrip
+    jetbrains.pycharm-professional
+    jetbrains.datagrip
     jetbrains.goland
     #jetbrains.phpstorm
     #jetbrains.webstorm
